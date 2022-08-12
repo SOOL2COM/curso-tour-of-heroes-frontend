73294c8d1f8b622be7b5e452f83bbef5a6c97678
Mayron Rodrigues de Almeida <mayronrjbr@gmail.com>
2022-08-12 19:45:37 -0300

    Versão Inicial

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'Curso Tour of Heroes';
+  subtitle = 'Mayron';
+}
